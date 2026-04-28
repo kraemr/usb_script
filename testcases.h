0dfbfab9d878888bc984b1b38b82e67d2b659b36
rob <93094049+kraemr@users.noreply.github.com>
2026-04-28 18:08:46 +0200

    delete testcases.h

--- testcases.h
+++
@@ -1,8 +0,0 @@
-#include "parser.h"
-typedef struct TestCase {
-	const char* name;
-	const char* str;
-	UsbCommand expected_commands[8];
-	size_t commands_count;
-	PARSING_STATE expected_state;
-}TestCase;
